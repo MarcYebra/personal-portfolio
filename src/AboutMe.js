2214f3ae43041fedf903db559db0201dfbcc0c88
Marc Yebra <106407943+MarcYebra@users.noreply.github.com>
2023-02-21 15:29:08 -0600

    Basic setup for About page

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -3,8 +3,29 @@ import React from 'react'
 const AboutMe = ( ) => {
 
   return(
-    <div>
-      <h1>About Me</h1>
+    <div className='about-me-container'>
+      <div className='text-color-white text-center'>
+        <h1>About</h1>
+      </div>
+
+      <div className='text-color-white'>
+        <h4>Hi, my name is</h4>
+      </div>
+
+      <div className='text-color-white'>
+        <h1>Marc Yebra.</h1>
+      </div>
+
+      <div className='text-color-white'>
+        <h1>I create web applications.</h1>
+      </div>
+
+      <div className='text-color-white'>
+        <p>I'm a fullstack web developer based in the Greater Boston Area. Currently, I'm focused on building 
+          front-end applications that incorporate UI/UX design technologies to enhance user experience.
+        </p>
+      </div>
+
     </div>
   )
 } 
